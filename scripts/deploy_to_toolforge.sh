edbef586096c335fff8cbdeb565dc5f59d269bf2
Hal Triedman <htriedman@wikimedia.org>
2024-05-30 11:13:46 -0600

    trying various deployment strategies, will clean up later

--- scripts/deploy_to_toolforge.sh
+++ scripts/deploy_to_toolforge.sh
@@ -6,10 +6,12 @@ user=${1-$USER}
 
 npm install
 npm run build
+chmod -R +w dist
+chgrp -R tools.dp-pageviews dist
 scp -r dist $user@login.toolforge.org:/data/project/dp-pageviews/dist
 ssh $user@login.toolforge.org "
   set -ex
-  become wlh bash -xc \"
+  become dp-pageviews bash -xc \"
     take dist &&
     set -e &&
     rm -rf public_html.bak &&
